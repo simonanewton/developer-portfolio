e016cf4e17ca059aafc736ab2677ccd9df189f57
Simon <simonanewton@gmail.com>
2022-06-14 22:18:55 -0400

    refactored updateCountryData, added country flag next to name

--- client/src/components/CountryBlock/index.js
+++ client/src/components/CountryBlock/index.js
@@ -10,6 +10,7 @@ class CountryBlock extends Component {
             apiKey: "",
             countryData: {
                 countryName: null,
+                countryFlag: null,
                 countryCaptial: null,
                 countryRegion: null,
                 countryLanguages: null,
@@ -20,30 +21,16 @@ class CountryBlock extends Component {
         }
     }
 
-    getRandomCountry = () => {
-        return countriesArray[Math.floor(Math.random() * countriesArray.length)];
-    }
-
-    formatLanguages = (languages) => {
-        return Object.values(languages)[0];
-    }
-
-    formatCurrencies = (currencies) => {
-        var currencyObject = Object.values(currencies)[0];
-        var currencyValue = Object.values(currencyObject)[0];
-        return currencyValue
-            .toLowerCase()
-            .split(' ')
-            .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
-            .join(' ');
-    }
-
-    formatPopulation = (population) => {
-        return population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
-
     updateCountryData = () => {
         const randomCountry = countriesArray[Math.floor(Math.random() * countriesArray.length)];
+
+        const toUpperLetter = (currencies) => {
+            return currencies
+                .toLowerCase()
+                .split(' ')
+                .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+                .join(' ');
+        }
 
         fetch(`https://restcountries.com/v3.1/name/${randomCountry}`)
             .then(response => response.json())
@@ -51,19 +38,19 @@ class CountryBlock extends Component {
                 this.setState({
                     countryData: {
                         countryName: data[0].name.common,
+                        countryFlag: Object.values(data[0].flags)[0],
                         countryCaptial: data[0].capital[0],
                         countryRegion: data[0].region,
-                        countryLanguages: this.formatLanguages(data[0].languages),
-                        countryCurrencies: this.formatCurrencies(data[0].currencies),
-                        countryPopulation: this.formatPopulation(data[0].population),
-                        countryLink: "https://en.wikipedia.org/wiki/" + data[0].name.common
+                        countryLanguages: (Object.values(data[0].languages)).join(", "),
+                        countryCurrencies: toUpperLetter(Object.values(Object.values(data[0].currencies)[0])[0]),
+                        countryPopulation: data[0].population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+                        countryLink: `https://en.wikipedia.org/wiki/${data[0].name.common}`
                     }
                 });
-                return;
             })
             .catch(error => {
                 console.log(error);
-                return;
+                return error;
             });
     }
 
@@ -88,9 +75,12 @@ class CountryBlock extends Component {
                             </div>
                         </Col>
                         <Col>
-                            <Row className="py-4 px-5 no-gutters">
-                                <Col>
-                                    <h3 className="my-2">{this.state.countryData.countryName}</h3>
+                            <Row className="py-4 px-5 no-gutters align-items-center">
+                                <Col className="pr-4">
+                                    <div className="my-2 d-flex align-items-center">
+                                        <h4 className="mr-3 m-0">{this.state.countryData.countryName}</h4>
+                                        <img src={this.state.countryData.countryFlag} alt="Country Flag" width="40" height="25" />
+                                    </div>
                                     <hr className="bg-dark ml-0" style={{ width: "50%" }} />
                                     <div className="d-flex align-middle">
                                         <h5 className="mr-2 font-weight-bold text-nowrap">Captial City:</h5>
@@ -101,7 +91,7 @@ class CountryBlock extends Component {
                                         <h5 className="d-inline-block">{this.state.countryData.countryRegion}</h5>
                                     </div>
                                     <div className="d-flex align-middle">
-                                        <h5 className="mr-2 font-weight-bold text-nowrap">Official Language:</h5>
+                                        <h5 className="mr-2 font-weight-bold text-nowrap">Official Languages:</h5>
                                         <h5 className="d-inline-block">{this.state.countryData.countryLanguages}</h5>
                                     </div>
                                     <div className="d-flex align-middle">
