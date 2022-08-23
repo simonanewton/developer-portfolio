7e6daebc8466e511627e60787c2ad936874372fe
Simon <simonanewton@gmail.com>
2022-08-22 21:34:41 -0400

    updated react package, substituted reportWebVitals

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
-import * as serviceWorker from "./serviceWorker";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
 
-serviceWorker.register();
+reportWebVitals();
